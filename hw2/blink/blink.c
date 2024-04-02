0fc2c3c38728115b11a3ac9004a3eb406f0b7277
Nick Marchuk <ndm736@users.noreply.github.com>
2024-04-01 21:32:29 -0500

    blink complies

---
+++ hw2/blink/blink.c
@@ -0,0 +1,23 @@
+#include "pico/stdlib.h"
+
+// The LED is connected to GPIO 25
+#define LED_PIN 25
+
+// Main (runs on core 0)
+int main() {
+    // Initialize the LED pin
+    gpio_init(LED_PIN);
+    // Configure the LED pin as an output
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // Loop
+    while (true) {
+        // Set high
+        gpio_put(LED_PIN, 1);
+        // Sleep
+        sleep_ms(500);
+        // Set low
+        gpio_put(LED_PIN, 0);
+        // Sleep
+        sleep_ms(500);
+    }
+}
---
+++ hw2/build/blink/CMakeFiles/blink.dir/DependInfo.cmake
@@ -0,0 +1,156 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_divider/divider.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_divider/divider.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_divider/divider.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_divider/divider.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_aeabi.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_aeabi.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_aeabi.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_aeabi.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S.obj"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/crt0.S" "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/build/blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/crt0.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "LIB_PICO_BIT_OPS=1"
+  "LIB_PICO_BIT_OPS_PICO=1"
+  "LIB_PICO_DIVIDER=1"
+  "LIB_PICO_DIVIDER_HARDWARE=1"
+  "LIB_PICO_DOUBLE=1"
+  "LIB_PICO_DOUBLE_PICO=1"
+  "LIB_PICO_FLOAT=1"
+  "LIB_PICO_FLOAT_PICO=1"
+  "LIB_PICO_INT64_OPS=1"
+  "LIB_PICO_INT64_OPS_PICO=1"
+  "LIB_PICO_MALLOC=1"
+  "LIB_PICO_MEM_OPS=1"
+  "LIB_PICO_MEM_OPS_PICO=1"
+  "LIB_PICO_PLATFORM=1"
+  "LIB_PICO_PRINTF=1"
+  "LIB_PICO_PRINTF_PICO=1"
+  "LIB_PICO_RUNTIME=1"
+  "LIB_PICO_STANDARD_LINK=1"
+  "LIB_PICO_STDIO=1"
+  "LIB_PICO_STDIO_UART=1"
+  "LIB_PICO_STDLIB=1"
+  "LIB_PICO_SYNC=1"
+  "LIB_PICO_SYNC_CRITICAL_SECTION=1"
+  "LIB_PICO_SYNC_MUTEX=1"
+  "LIB_PICO_SYNC_SEM=1"
+  "LIB_PICO_TIME=1"
+  "LIB_PICO_UTIL=1"
+  "PICO_BOARD=\"pico\""
+  "PICO_BUILD=1"
+  "PICO_CMAKE_BUILD_TYPE=\"Debug\""
+  "PICO_COPY_TO_RAM=0"
+  "PICO_CXX_ENABLE_EXCEPTIONS=0"
+  "PICO_NO_FLASH=0"
+  "PICO_NO_HARDWARE=0"
+  "PICO_ON_DEVICE=1"
+  "PICO_TARGET_NAME=\"blink\""
+  "PICO_USE_BLOCKED_RAM=0"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_stdlib/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_gpio/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_base/include"
+  "generated/pico_base"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/boards/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_platform/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2040/hardware_regs/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_base/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2040/hardware_structs/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_claim/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_sync/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_timer/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_uart/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_resets/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_clocks/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_pll/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_vreg/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_watchdog/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_xosc/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_divider/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_runtime/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_printf/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_bit_ops/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_divider/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_malloc/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bootrom/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_binary_info/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio_uart/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_int64_ops/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_mem_ops/include"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/boot_stage2/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/critical_section.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/critical_section.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/critical_section.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/lock_core.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/lock_core.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/lock_core.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/mutex.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/mutex.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/mutex.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/sem.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/sem.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_sync/sem.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/time.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/time.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/time.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/timeout_helper.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/timeout_helper.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_time/timeout_helper.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/datetime.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/datetime.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/datetime.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/pheap.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/pheap.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/pheap.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/queue.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/queue.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/common/pico_util/queue.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_claim/claim.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_claim/claim.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_claim/claim.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/irq.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/irq.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_irq/irq.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_pll/pll.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_pll/pll.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_pll/pll.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_sync/sync.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_sync/sync.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_sync/sync.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_timer/timer.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_timer/timer.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_timer/timer.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_uart/uart.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_uart/uart.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_uart/uart.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_math.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_math.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_platform/platform.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_platform/platform.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_platform/platform.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_printf/printf.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_printf/printf.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_printf/printf.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d"
+  "/Users/nickmarchuk/Documents/GitHub/me433_sandbox/hw2/blink/blink.c" "blink/CMakeFiles/blink.dir/blink.c.obj" "gcc" "blink/CMakeFiles/blink.dir/blink.c.obj.d"
+  "/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj" "gcc" "blink/CMakeFiles/blink.dir/Users/nickmarchuk/me433/2024/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
